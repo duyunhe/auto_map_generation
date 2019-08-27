d9cff7472c4c8ef82b3ea470f5ceed3ae4f7df3f
duyunhe <a_crux@126.com>
2019-08-27 11:20:58 +0800

    Initial commit

---
+++ common.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/27 11:19
+# @Author  : yhdu@tongwoo.cn
+# @简介    : 
+# @File    : common.py
---
+++ gene_map.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/27 11:08
+# @Author  : yhdu@tongwoo.cn
+# @简介    : 
+# @File    : gene_map.py
